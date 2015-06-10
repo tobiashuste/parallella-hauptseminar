66d1c1d7b1b1d904a4216fc07d1d340115200fb4
tobiasfrust <tobiasfrust@Tobias-MacBook-Pro.local>
2015-06-10 12:25:43 +0200

    Folder renamed

--- fftParallela/host.cpp
+++ fftParallela/host.cpp
@@ -14,7 +14,6 @@ int main(int argc, char* argv[]){
 	int sizeX, sizeY;
 	char *s;	
 
-	//TODO: nutzereingabe lesen
 	if(argc != 4){
 		printf("usage: executable <name of input data> <imageWidth> <imageLength>");
 		exit(1);
@@ -38,6 +37,32 @@ int main(int argc, char* argv[]){
 	e_epiphany_t dev;
 
 	e_init(nullptr); //identisch zu NULL?
+    e_reset_system();
+    e_get_platform_info(&platform);
+    
+    //start workgroup with all e-cores
+    if(e_open(&dev, 0, 0, platform.rows, platform.cols) != E_OK){
+        printf("e_open failed\n");
+        exit(1);
+    }
+    
+    //load epiphany program into workgroup, but don't start it yet
+    /*if(e_load_group(epiphanyExecutable, &dev, 0, 0, platform.rows, platform.cols, E_FALSE) == E_ERR){
+        printf("epiphany program could not be loaded into workgroup\n");
+        exit(1);
+    }*/
+    
+    //TODO: Korrekte Aufteilung der Daten
+    //copy data to epiphany device
+    /*for(int i = 0; i < sizeY/(platform.rows*platform.cols); i++){
+        for(int row = 0; row < platform.rows; row++){
+            for(int col = 0; col < platform.cols; col){
+                e_write(&dev, row, col, 0x200, sizeof(float)*...*...);
+            }
+        }
+    e_start_group(&dev);
+     //TODO: check if epiphany is ready, copy results back
+    }*/
 	
 	writeTiff((char*)"out.tif", img, sizeY, sizeX);
 
